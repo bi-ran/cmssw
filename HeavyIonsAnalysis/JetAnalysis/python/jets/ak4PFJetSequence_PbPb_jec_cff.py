d17454e3d16a62a961f2fb33d92cd212146daa52
R. Alex Barbieri <luck@mit.edu>
2016-03-10 12:43:51 -0500

    add Filter and SoftDrop to jet sequences

--- HeavyIonsAnalysis/JetAnalysis/python/jets/ak4PFJetSequence_PbPb_jec_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/jets/ak4PFJetSequence_PbPb_jec_cff.py
@@ -193,7 +193,8 @@ ak4PFJetAnalyzer = inclusiveJetAnalyzer.clone(jetTag = cms.InputTag("ak4PFpatJet
                                                              bTagJetName = cms.untracked.string("ak4PF"),
                                                              genPtMin = cms.untracked.double(15),
                                                              hltTrgResults = cms.untracked.string('TriggerResults::'+'HISIGNAL'),
-							     doTower = cms.untracked.bool(True)
+							     doTower = cms.untracked.bool(True),
+							     doSubJets = cms.untracked.bool(False)
                                                              )
 
 ak4PFJetSequence_mc = cms.Sequence(
